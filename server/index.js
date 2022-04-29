ef4622f43b7378b2a546342d5954e76a871314de
Jennifer Chou <63386589+codecaviette@users.noreply.github.com>
2022-04-29 10:41:55 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,7 @@
+// This is the starting point of our server application
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
